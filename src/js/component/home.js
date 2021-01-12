3ebc13a8e2c37579509cfde9049a0712905267c8
kedwards413 <kaela.rose.edwards@gmail.com>
2021-01-12 01:58:49 +0000

    style edits

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,27 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 
 export const Home = () => {
 	const [color, setColor] = useState("");
-
+	useEffect(
+		() => {
+			const interval = setInterval(() => {
+				color === "red"
+					? setColor("green")
+					: color === "yellow"
+						? setColor("red")
+						: color === "green"
+							? setColor("yellow")
+							: null;
+			}, 1000);
+			return () => clearInterval(interval);
+		},
+		[color]
+	);
 	return (
-		<div className="container d-flex">
+		<div className="container d-flex justify-content-center pt-5">
 			<div className="trafficLight">
-					<div
-						className={color === "red" ? "redOn" : "redOff"}
-						onClick={() => setColor("red")}
-					/>
-					<div
-						className={
-							color === "yellow" ? "yellowOn" : "yellowOff"
-						}
-						onClick={() => setColor("yellow")}
-					/>
-					<div
-						className={color === "green" ? "greenOn" : "greenOff"}
-						onClick={() => setColor("green")}
-					/>
+				<div
+					className={color === "red" ? "redOn" : "redOff"}
+					onClick={() => setColor("red")}
+				/>
+				<div
+					className={color === "yellow" ? "yellowOn" : "yellowOff"}
+					onClick={() => setColor("yellow")}
+				/>
+				<div
+					className={color === "green" ? "greenOn" : "greenOff"}
+					onClick={() => setColor("green")}
+				/>
 			</div>
 		</div>
 	);
